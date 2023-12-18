30685c38d8543adb4348de4f212b8b83c6751884
noahgiboney <noahgiboney@gmail.com>
2023-12-18 12:29:48 -0800

    feat: responsive nav bar with slider

--- src/components/NavComponent.tsx
+++ src/components/NavComponent.tsx
@@ -1,17 +1,33 @@
 import React from "react";
 import style from "../styles/navbar.module.css";
 import Link from "next/link";
+import {FaBars, FaTimes} from 'react-icons/fa'
+import { useRef } from "react";
 
 export default function NavBar() {
+
+  const navRef = useRef<HTMLDivElement>(null);
+
+  const showNavBar = () => {
+    navRef.current?.classList.toggle(style.responsiveNav)
+  }
+
   return (
     <header className={style.navigation}>
-    <ul> 
-        <li><Link href={'/'}>Home</Link></li>
-        <li><Link href={'/#about'}>About</Link></li>
-        <li><Link href={'/#portfolio'}>Portfolio</Link></li>
-        <li><Link href={'/#blogs'}>Blog</Link></li>
-        <li><Link href={'/#contact'}>Contact</Link></li>
-    </ul>
+      <h1>NG</h1>
+      <nav ref={navRef}> 
+          <Link href={'/'} onClick={showNavBar}>Home</Link>
+          <Link href={'/#about'} onClick={showNavBar}>About</Link>
+          <Link href={'/#portfolio'} onClick={showNavBar}>Portfolio</Link>
+          <Link href={'/#blogs'} onClick={showNavBar}>Blog</Link>
+          <Link href={'/#contact'} onClick={showNavBar}>Contact</Link>
+          <button className={`${style.navBtn} ${style.navCloseBtn}`} onClick={showNavBar}>
+            <FaTimes/>
+          </button>
+      </nav>
+      <button className={style.navBtn} onClick={showNavBar}>
+        <FaBars/>
+      </button>
     </header>
   );
 }

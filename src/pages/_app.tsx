da48335acb045565da46cace64e2e937cddba306
noahgiboney <noahgiboney@gmail.com>
2023-12-19 09:51:33 -0800

    update: random styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <RootLayout>
       <Head>
         <title>Noah Giboney Portfolio</title>
-        <meta name="description" content="Personal Website For Noah Giboney" />
+        <meta name="description" content="Noah Giboney's Personal Website "/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />

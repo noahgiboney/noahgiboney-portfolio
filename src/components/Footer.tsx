6f9c8a81b7c3a3e6c093bed8ec148e1395502e67
noahgiboney <noahgiboney@gmail.com>
2023-12-18 18:22:00 -0800

    update: footer text:

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ import GitHub from 'images/icons/github.png'
 export default function Footer() {
   return (
    <footer className={style.footer}>
-        <p>noahgiboney © 2023</p>
+        <p>Created by Noah Giboney © 2023</p>
                 <a href="https://www.linkedin.com/in/noah-giboney-896847261/"><img src={LinkedIn.src} className="round"/></a>
                 <a href="https://github.com/noahgiboney"><img src={GitHub.src} className="round"/></a>
     </footer>

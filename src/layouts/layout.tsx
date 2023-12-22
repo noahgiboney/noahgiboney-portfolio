b4a78781310fec9957e02046bb8c40a378106d56
noahgiboney <noahgiboney@gmail.com>
2023-12-22 13:06:47 -0800

    update: font weight

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -3,7 +3,7 @@ import NavBar from '@/components/NavBar';
 import Footer from '@/components/Footer';
 import Headroom from 'react-headroom'
 
-const jose = Josefin_Sans({ subsets: ['vietnamese'], weight: ['300']})
+const jose = Josefin_Sans({ subsets: ['vietnamese'], weight: ['100']})
 
 export default function RootLayout({
   children,

20e6c2ed329bca7818514549ff0a8dbba7fdc6dd
noahgiboney <noahgiboney@gmail.com>
2023-12-22 16:31:30 -0800

    update: font weight from 100 to 200

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -3,7 +3,7 @@ import NavBar from '@/components/NavBar';
 import Footer from '@/components/Footer';
 import Headroom from 'react-headroom'
 
-const jose = Josefin_Sans({ subsets: ['vietnamese'], weight: ['100']})
+const jose = Josefin_Sans({ subsets: ['vietnamese'], weight: ['200']})
 
 export default function RootLayout({
   children,

49a9b5ae6fe591ab8e2805cbf5522d338a5abacb
noahgiboney <noahgiboney@gmail.com>
2023-12-22 19:56:07 -0800

    update: blog card styling

--- src/components/BlogPreviewComponent.tsx
+++ src/components/BlogPreviewComponent.tsx
@@ -11,9 +11,10 @@ const BlogPreviewComponent: React.FC<BlogComponentProps> = ({ blogs }) => {
   return (
     <div className={style.blogContainer}>
       {blogs.map(blog => (
-        <Link href={`/blogs/${blog.slug}`} key={blog.slug} className={style.blog}>
-            <h2 className={style.blogTitle}>{blog.title}</h2>
-            <p className={style.blogDate}>{blog.date}</p>
+        <Link href={`/blogs/${blog.slug}`} key={blog.slug} className={style.blogCard}>
+              <h2>{blog.title}</h2>
+              <p className={style.blogDate}>{blog.date}</p>
+            <div className={style.blogLine}></div>
             <p>{blog.description}</p>
         </Link>
       ))}

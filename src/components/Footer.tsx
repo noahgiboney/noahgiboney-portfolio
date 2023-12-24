d88edf64f5ee5fd8cd320f6f11d84006d7ff76d8
noahgiboney <noahgiboney@gmail.com>
2023-12-23 21:44:03 -0800

    update: footer, removed images

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,6 @@ export default function Footer() {
   return (
    <footer className={style.footer}>
         <p>Created by Noah Giboney © 2023</p>
-                <a href="https://www.linkedin.com/in/noah-giboney-896847261/"><img src="/images/icons/linkedin.png" className="round"/></a>
-                <a href="https://github.com/noahgiboney"><img src="/images/icons/github.png" className="round"/></a>
-    </footer>
+   </footer>
   );
 }

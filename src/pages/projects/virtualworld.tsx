14559a980d31b8e14edcd30acec78e7d965c1524
noahgiboney <noahgiboney@gmail.com>
2023-12-23 19:17:58 -0800

    fix: typos in virtual world p

--- src/pages/projects/virtualworld.tsx
+++ src/pages/projects/virtualworld.tsx
@@ -100,7 +100,7 @@ const aStarAlgo =
 
         <section className={style.subSection}>
           <h2 id='one'>Overview</h2>
-          <p>This virutal world was a quater long project in CSC203. Throughout the quater we applied concepts that we learned to a project that had a large exsisting code base.</p>
+          <p>This virtual world was a quarter-long project in CSC203. Throughout the quarter we applied concepts that we learned and refactored an existing code base.</p>
           <br></br>
           <br></br>
           <a className="button" href="https://github.com/noahgiboney/virtualworld" target="_blank" rel="noopener noreferrer">Repository</a>
@@ -108,11 +108,7 @@ const aStarAlgo =
 
         <section className={style.subSection}>
           <h2 id='two'>Demo</h2>
-          <p>The project in action. I added a world changing event that is triggered by the user pressing the volcano, which causes lava to flood the river
-           and wakes up spiders from the various nests across the world. The spiders will path to the human and kill them. This will cause the fairy to 
-           either choose to first kill the spiders or revive the, whichever is closest. This event can be triggered as many times as the user wants to keep
-           flooding the river and spawning spiders.
-          </p>
+          <p>Here is the project running. I added a world-changing event that is triggered by the user clicking on a volcano, which causes lava to flood the river and wake up spiders from the various nests across the world. The spiders target the human and kill them. This will cause the fairy to either choose to target the spider and kill it, or target the dude and revive them. The fairy will choose based on which entity is closest. This event can be triggered as many times as the user wants to keep flooding the river and spawning spiders.</p>
           <div className={style.imageContainer}>
             <VideoPlayer src="/videos/VirtualWorldDemo.mp4"/>
           </div>
@@ -120,31 +116,21 @@ const aStarAlgo =
 
         <section className={style.subSection}>
           <h2 id='three'>Pathfinding</h2>
-          <p>With the given code base, the entities had a problematic movement, they only moved in a single step fashion where they check the 
-            neighboring points without consiering the full path. This resulted them in getting stuck from moving quite often. To combat this, 
-            I implemented the a-star pathing algorithm</p>
+          <p>With the given code base, the entities had a problematic movement, they only moved in a single-step fashion where they checked neighboring points without considering the full path. This resulted in them getting stuck from moving quite often. To combat this, I implemented the a-star pathing algorithm.</p>
           <br></br>
-          <p>By calculating the total cost of a path as the sum of the actual start-to-node cost (g(n)) and the estimated node-to-goal cost (h(n)), 
-            A* intelligently prioritizes nodes that seem closer to the goal. The algorithm iteratively explores adjacent nodes, expanding its search 
-            based on the lowest combined cost (f(n) = g(n) + h(n)). Its effectiveness hinges on the heuristic's accuracy: a well-chosen heuristic can 
-            significantly reduce the search area, making a-star the fastest. It never overestimates the actual cost to the goal, a-star ensures the 
-            discovery of the shortest path.</p>
+          <p>By calculating the total cost of a path as the sum of the actual start-to-node cost (g(n)) and the estimated node-to-goal cost (h(n)), a-star intelligently prioritizes nodes that seem closer to the goal. The algorithm iteratively explores adjacent nodes, expanding its search based on the lowest combined cost (f(n) = g(n) + h(n)). Its effectiveness hinges on the heuristic's accuracy: a well-chosen heuristic can significantly reduce the search area, making a-star the fastest. It never overestimates the actual cost to the goal, therefore a-star ensures the discovery of the shortest path.</p>
           <br></br>
-          <p>This is my function 'computePath' that is the implementation of the a-star algorithm for this virtual world. It takes in a stream of 
-            potential neighboring points, a predicate to determine if the point is valid to move to, and a biprediate if the point is within reach of
-            the entity. I used a linkedlist to store the path and a priority queue hold open points, sorted by the fCost then the gCost. I also used a
-            HashSet for the closed list for quick lookup times.
-          </p>
+          <p>This is my function 'computePath' which is the implementation of the a-star algorithm that the entities use in this virtual world. It takes in a stream of potential neighboring points, a predicate to determine if the point is valid to move to, and a biprediate if the point is within reach of the entity. I used a LinkedList to store the path and a priority queue to hold open points, sorted by the fCost and then the gCost. I also used a HashSet for the closed list for quick lookup times.</p>
            <CodeSnippet code={aStarAlgo} language="java"/>
         </section>
 
         <section className={style.subSection}>
           <h2 id='four'>Refactoring</h2>
-          <p>The given code base had many problems that we were tasked to fix. We first had to refactor without changing the function of any entities or their interactoin. Firstly, the classes had high coupling and low cohesion. They also relied on an enum rather the using instances of objects. There were many correct ways to approach the refactor, but the following UML represents my design.</p>
+          <p>This project called for refactoring the codebase without disrupting the function of the game. The classes had high coupling and low cohesion. They also relied on an enum rather the using instances of entity objects. There were many correct ways to approach the refactor, but the following UML represents my design.</p>
           <div className={style.imageContainer}>
-            <img src="/images/virtualworld/UML.png" alt='uml diagram'></img>
+          <img src="/images/virtualworld/UML.png" alt='uml diagram'></img>
           </div>
-          <p>In order to achieve high cohesion, moving around instance variables was essential, along with creating other classes to create a entity heiarchy. Having parent abstract that acted as super classes to similar behaving entities reduced repition in the code base and allowed for a concise design. Interfaces also were helpful to refactor, as they allowed me to pull a common method from classes and only have a certain class implement the trasnfrom interface if they can do this in the game.</p>
+          <p>To achieve high cohesion, moving around instance variables was essential to their corresponding classes, along with creating other classes to create an entity hierarchy. Implementing abstracts classes to similar behaving entities reduced repetition and allowed for a concise design. Interfaces also were helpful in refactoring, as they allowed me to pull a common method from classes if they had a shared behavior. </p>
         </section>
       </section>
     );

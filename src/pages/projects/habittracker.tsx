2b584ce149981fae814b477c48b0d2217be7bc7a
noahgiboney <noahgiboney@gmail.com>
2024-01-01 20:58:13 -0800

    feat: added habit tracker preview and slug

---
+++ src/pages/projects/habittracker.tsx
@@ -0,0 +1,9 @@
+import style from '@/styles/projects.module.css'
+
+export default function Home() {
+    return (
+      <section className={style.mainContainer}>
+        <p>Coming Soon</p>
+      </section>
+    );
+  }
